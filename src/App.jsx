f718b0aaa95d8572534326fc43b073fc6187fc71
Caio Bruno <caiobruno90@yahoo.com>
2023-12-28 14:39:29 -0300

    Estrutura do componete principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
+//componets
+import {GrFormNext, GrFormPrevious} from 'react-icons/gr'
 import './App.css'
 
 function App() {
   
   return (
     <>
-       <h1>MultiStep</h1>
+      
+       <div className="header">
+        <h2>Deixe sua avaliaç;ao</h2>
+        <p>Ficamos felizes com a sua compra, ultilizer o formulario abaixo para avaçiar o produto</p>
+       </div>
+       <div className="form-container">
+        <p>etapas</p>
+        <form >
+          <div className="inputs-container">
+            
+          </div>
+          <div className="actions">
+            <button type='button'>
+              <GrFormPrevious/>
+              <span>Voltar</span>
+              </button>
+            <button type='submit'>
+              <span>Avançar</span>
+               <GrFormNext/>
+              </button>
+          </div>
+        </form>
+       </div>
     </>
   )
 }
