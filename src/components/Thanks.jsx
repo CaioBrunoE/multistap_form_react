35012c18b1ace39d58bee618582fec2349e577f0
Caio Bruno <caiobruno90@yahoo.com>
2023-12-28 23:08:15 -0300

    Componetente de envio de avaliação

--- src/components/Thanks.jsx
+++ src/components/Thanks.jsx
@@ -1,8 +1,26 @@
+import {
+  BsFillEmojiHeartEyesFill,
+  BsFillEmojiSmileFill,
+  BsFillEmojiNeutralFill,
+  BsFillEmojiFrownFill,
+} from "react-icons/bs";
 
+import './Thanks.css'
 
 function Thanks() {
   return (
-    <div><h2>Thanks</h2></div>
+    <div className="thanks-container">
+      <h2>Falta pouco...</h2>
+      <p>A sua opniao e muito importante, em breve voce recebera um cupom de 10% de desconto.</p>
+      <p>Para concluir a sua avaliação click no botão de enviar abaixo</p>
+      <h3>Aqui esta o resumo da sua avaliação</h3>
+      <p className="revieew-data">
+        <span>Satisfação com o produto</span>
+      </p>
+      <p className="revieew-data">
+        <span>Comentario:</span>
+      </p>
+    </div>
   )
 }
 
